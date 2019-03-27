80dc49505dbf4d687dae273fca8c2ae7fc31c7aa
Parth Nagori <nagori.parth@gmail.com>
2019-03-27 00:32:45 -0400

    Final 2 Thread version

--- pthread_sort.cpp
+++ pthread_sort.cpp
@@ -20,6 +20,7 @@ float *arr;
 int n;
 int part = 0;
 
+// Struct to pass range and memory information to each thread
 struct partition {
     int l;
     int r;
@@ -27,18 +28,12 @@ struct partition {
     float *right;
 };
 
-
+// Merge operation
 void merge(int l, int m, int r, float* left, float* right)
 {
-  //printf("Inside Merge Range: %d %d %d\n", l, m, r);
-  //printf("Inside Merge \n");
-
   int lsize = m - l + 1;
   int rsize = r - m;
   int i, j, k;
-
-  // float *left_subarray = (float *)malloc(lsize * sizeof(float));
-  // float *right_subarray = (float *)malloc(rsize * sizeof(float));
 
   float *left_subarray = left;
   float *right_subarray = right;
@@ -83,15 +78,12 @@ void merge(int l, int m, int r, float* left, float* right)
     k++;
   }
 
-  // free(left_subarray);
-  // free(right_subarray);
 }
 
-
+// Recursive Merge Sort
 void merge_sort(int l, int r, float* left, float* right)
 {
 
-  // printf("Inside merge sort \n");
   int m = l + ((r - l) / 2);
   if (l < r) {
     merge_sort(l, m, left, right);
@@ -100,10 +92,9 @@ void merge_sort(int l, int r, float* left, float* right)
   }
 }
 
+// Call Merge Sort operation for each thread
 void* merge_sort_parallel(void* arg)
 {
-  // printf("Inside Merge parallel \n");
-
   struct partition *partition = (struct partition *)arg;
   int l;
   int r;
@@ -124,11 +115,9 @@ void* merge_sort_parallel(void* arg)
 int pthread_sort(int num_of_elements, float *data)
 {
 
-  // printf("Inside pthread \n");
   arr = data;
   n = num_of_elements;
 
-  // struct partition *partition;
   struct partition* partition1 = (struct partition*) malloc(sizeof(struct partition));
   struct partition* partition2 = (struct partition*) malloc(sizeof(struct partition));
 
@@ -136,9 +125,6 @@ int pthread_sort(int num_of_elements, float *data)
 
   int partition_length = n / THREAD_COUNT;
 
-  // printf("THREADS:%d MAX:%d partition_length:%d\n", THREAD_COUNT, n, partition_length);
-
-  
   int l = 0;
 
   // Generating partition values for partitions to be run on both the threads
@@ -148,7 +134,7 @@ int pthread_sort(int num_of_elements, float *data)
   partition2->l = l + partition_length;
   partition2->r = n - 1;
 
-
+  // Allocating a common memory location for each thread
   partition1->left = (float *)malloc(partition_length * sizeof(float));
   partition1->right = (float *)malloc(partition_length * sizeof(float)); 
 
@@ -158,7 +144,6 @@ int pthread_sort(int num_of_elements, float *data)
 
   
   int rc;
-  // printf("main: begin\n");
   rc = pthread_create(&p1, NULL, merge_sort_parallel, (void *) partition1); assert(rc == 0);
   rc = pthread_create(&p2, NULL, merge_sort_parallel, (void *) partition2); assert(rc == 0);
 
@@ -168,9 +153,6 @@ int pthread_sort(int num_of_elements, float *data)
 
 
   merge(partition1->l, partition2->l - 1, partition2->r, partition1->left, partition1->right);
-
-  // free(partition1);
-  // free(partition2);
 
   return 0;
 
